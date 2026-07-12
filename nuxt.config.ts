4f748d9c0708de38518ad627b7266ad42692ca9a
Mattisbbn <mattisbbn@gmail.com>
2026-07-12 14:38:34 +0200

    Resolve css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from '@nuxt/kit'
+const { resolve } = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/image', '@nuxt/ui','nuxt-auth-utils'],
-  css: ['~/assets/css/main.css']
+  css: [resolve('./assets/css/main.css')],
 })
